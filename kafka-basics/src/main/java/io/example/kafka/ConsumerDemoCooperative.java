9cd37857d1d1a31bfc94140162af59528bb78483
michael-marlon <michael.marlon@gmail.com>
2024-04-30 19:00:19 -0400

    feat: Cooperative Consumer

--- kafka-basics/src/main/java/io/example/kafka/ConsumerDemoCooperative.java
+++ kafka-basics/src/main/java/io/example/kafka/ConsumerDemoCooperative.java
@@ -2,6 +2,7 @@ package io.example.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -12,9 +13,9 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 
-public class ConsumerDemoWithShutdown {
+public class ConsumerDemoCooperative {
 
-    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoWithShutdown.class.getSimpleName());
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoCooperative.class.getSimpleName());
 
     public static void main(String[] args) {
         log.info("I am a Kafka Consumer!");
@@ -33,6 +34,8 @@ public class ConsumerDemoWithShutdown {
         properties.setProperty("value.deserializer", StringDeserializer.class.getName());
         properties.setProperty("group.id", groupId);
         properties.setProperty("auto.offset.reset", "earliest"); //none/earliest/latest
+        properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
+//        properties.setProperty("group.instance.id", "...")
 
         // create a consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
